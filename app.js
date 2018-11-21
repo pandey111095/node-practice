7de0de52d7d75dc54447d292bee5597a1b00f3d9
Rahul kumar pandey <pandey111095@gmail.com>
2018-11-21 20:16:47 +0530

    Adding Feature for run on heroku server:- Set up start script in package.json and adding heroku port

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express');
+
+
+const port = process.env.PORT || 3000 ;
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('Ram is a good boy');
+});
 console.log('Ram');
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
