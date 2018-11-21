52adecab87825537efc73415b7e37ee657bd31dd
Rahul kumar pandey <pandey111095@gmail.com>
2018-11-21 20:42:07 +0530

    Added Project Project page and about page with hbs view engine

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const hbs = require('hbs');
 
 
 const port = process.env.PORT || 3000 ;
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', (req, res) => {
     res.send('Ram is a good boy');
 });
 console.log('Ram');
 
+app.get('/about', (req, res) => {
+    res.render('about.hbs');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pagetitle: 'Projects'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-})
+});
